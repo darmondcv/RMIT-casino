2c6a9fa4815a78c3bdd7c3133f11d662d5eb0def
Dang <darmondcv@gmail.com>
2024-08-05 15:07:04 +0700

    Initial Commit

---
+++ RMIT casino/RMIT_casinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_casinoApp.swift
+//  RMIT casino
+//
+//  Created by Darmond on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_casinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
